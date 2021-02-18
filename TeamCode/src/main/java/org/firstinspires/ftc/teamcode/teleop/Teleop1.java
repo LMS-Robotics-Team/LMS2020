8cc5057a04b7c94ac07e7b46cc6221bf836e450d
markfontecchio <58674223+markfontecchio@users.noreply.github.com>
2021-02-18 16:44:17 -0500

    update 2/18/21

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop1.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
-public class Teleop extends LinearOpMode {
+public class Teleop1 extends LinearOpMode {
 
     // sets variables for motors and servos
     private DcMotor driveFL, driveFR, driveBL, driveBR;
